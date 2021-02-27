560767af351178b0fe98c24c1f6bd4bbdb003a73
kaiz <merchant.kaiz@gmail.com>
2021-02-26 20:41:02 -0500

    experiment with user models,jsonresponse and uuid

--- scienceDMZ/portal/models.py
+++ scienceDMZ/portal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# class MyUsers(models.Model):
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#     )
--- scienceDMZ/portal/views.py
+++ scienceDMZ/portal/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+import uuid
 
 # Create your views here.
+
+
 def index(request):
+    # result = User.objects.get(username="admin")
+    # return HttpResponse(result.username)
     return render(request, 'portal/index.html', {})
 
+
 def welcome(request):
-    return HttpResponse("Welcome to the Portal")
+    container_id =  str(uuid.uuid4())
+    return JsonResponse({'c_id': container_id})
+
+    # return HttpResponse("Welcome to the Portal " + container_id)
