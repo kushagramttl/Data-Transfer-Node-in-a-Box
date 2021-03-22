1c47d04c9bad9ae7fbb62c0a62f7d4d1e759abe7
Guneetkaur21 <guneet2109@gmail.com>
2021-03-22 14:53:39 -0400

    Integration of sign in and registration with portal

--- ContainerScripts/Controller/main.py
+++ ContainerScripts/Controller/main.py
@@ -1,27 +1,46 @@
 import time
-from portalclient import get_commands
+from portalclient import get_commands, post_register_container, login
 from config import ConfigSingleton
 from transfercommand import initiate_transfer
-from multiprocessing import Process
+from multiprocessing import Process, Pool
 from testminio import main
 
 def initiate():
+
     ConfigSingleton.getInstance().set_config_values()
     wait_time = ConfigSingleton.getInstance().config_dict["WAIT_TIME"]
 
-    while(True):
-        command = get_commands(ConfigSingleton.getInstance().config_dict["PORTAL_URL"])
+    # to login into the container
+    login_url = ConfigSingleton.getInstance().config_dict["LOGIN_URL"]
+    session = login(login_url, "alice", "Dockerbaba")
 
-        if command["command"] == "START":
-            p = Process(target=initiate_transfer)
-            p.start()
-            p.join()
+    # for registration of conatiner with portal
+    container_registration = ConfigSingleton.getInstance().config_dict["CONTAINER_REGISTER_URL"]
+    post_register_container(container_registration, session)
+
+    while(True):
+        commands = get_commands(ConfigSingleton.getInstance().config_dict["FETCH_COMMAND_URL"], session)
+        print("In main : " , commands)
+        # for(command in commands):
+
+        # if command["command"] == "START":
+            # p = Process(target=initiate_transfer)
+            # p.start()
+            # p.join()
+        number_of_processes = len(commands)
+
+        with Pool(number_of_processes) as p:
+            results = p.map(initiate_transfer, commands)
+            print(results)
 
         time.sleep(wait_time)
 
         break
 
-    main()
+  
+
+
+    # main()
 
 if __name__ == '__main__':
     initiate()
