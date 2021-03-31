92b5c55c0451172c0cf916a786bbb4b40bed09db
changsu <suchang177@outlook.com>
2021-03-30 22:50:35 -0400

    network clean

--- Network/controller/main.py
+++ Network/controller/main.py
@@ -1,9 +1,8 @@
 import time
-from transferClient import transfer_list, post_transfer, update_status
+from transferClient import get_transfer_list, post_transfer, update_status
 from config import ConfigSingleton
 from ContainerScripts.Controller.portalclient import login
-
-def initiate():
+def initiate( session ):
 
     ConfigSingleton.getInstance().set_config_values()
     wait_time = ConfigSingleton.getInstance().config_dict["WAIT_TIME"]
@@ -13,7 +12,6 @@ def initiate():
 
     # username = input("Please enter your username: ")
     # pwd = input("Please enter your password: ")
-
     username = "admin"
     pwd = "admin"
 
@@ -21,8 +19,11 @@ def initiate():
 
     # to start a new transfer
     start_transfer = ConfigSingleton.getInstance().config_dict["INIT_TRANSFER_URL"]
-    post_transfer( start_transfer, session )
+    post_transfer( start_transfer, session , transferId)
 
+    transfers = get_transfer_list(ConfigSingleton.getInstance().config_dict["GET_TRANSFER_LIST_URL"], session)
+
+    update_transfer = update_status( ConfigSingleton.getInstance().config_dict["GET_TRANSFER_STATUS_URL"], session, transferId )
 
 if __name__ == '__main__':
     initiate()
