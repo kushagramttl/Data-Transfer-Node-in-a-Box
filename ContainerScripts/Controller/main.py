f267dd6eed706f81ee5c21e2b4abbabe011ed165
Guneetkaur21 <guneet2109@gmail.com>
2021-03-31 22:55:05 -0400

    Code refactor

--- ContainerScripts/Controller/main.py
+++ ContainerScripts/Controller/main.py
@@ -1,11 +1,13 @@
 import time
-from portalclient import get_commands, post_register_container, login
+from portalclient import PortalClient
 from config import ConfigSingleton
 from transfercommand import initiate_transfer
 from multiprocessing import Process, Pool
 from testminio import main
 
 def initiate():
+
+    portal_client = PortalClient()
 
     ConfigSingleton.getInstance().set_config_values()
     wait_time = ConfigSingleton.getInstance().config_dict["WAIT_TIME"]
@@ -17,43 +19,43 @@ def initiate():
     # pwd = input("Please enter your password: ")
 
     username = "alice"
-    pwd = "DockerBaba"
+    pwd = "Dockerbaba"
 
-    session = login(login_url, username, pwd)
+    session = portal_client.login(login_url, username, pwd)
 
-    # for registration of conatiner with portal
-    container_registration = ConfigSingleton.getInstance().config_dict["CONTAINER_REGISTER_URL"]
+    if(session is not None):
 
-    # access_key = input("Please enter your access key: ")
-    # secret_key = input("Please enter your secret key: ")
-    access_key = "test3"
-    secret_key = "test3"
-    post_register_container(container_registration, session, access_key, secret_key)
+        # for registration of conatiner with portal
+        container_registration = ConfigSingleton.getInstance().config_dict["CONTAINER_REGISTER_URL"]
 
-    while(True):
-        commands = get_commands(ConfigSingleton.getInstance().config_dict["FETCH_COMMAND_URL"], session)
-        if len(commands) > 0:
-            print("In main : ", commands)
-        # for(command in commands):
+        # access_key = input("Please enter your access key: ")
+        # secret_key = input("Please enter your secret key: ")
+        access_key = "test3"
+        secret_key = "test3"
+        portal_client.post_register_container(container_registration, session, access_key, secret_key)
 
-        # if command["command"] == "START":
-            # p = Process(target=initiate_transfer)
-            # p.start()
-            # p.join()
-            number_of_processes = len(commands)
+        while(True):
+            commands = portal_client.get_commands(ConfigSingleton.getInstance().config_dict["FETCH_COMMAND_URL"], session)
+            if len(commands) > 0:
+                print("In main : ", commands)
+            # for(command in commands):
 
-            with Pool(number_of_processes) as p:
-                results = p.map(initiate_transfer, commands)
-                print(results)
+            # if command["command"] == "START":
+                # p = Process(target=initiate_transfer)
+                # p.start()
+                # p.join()
+                number_of_processes = len(commands)
 
-        time.sleep(wait_time)
+                with Pool(number_of_processes) as p:
+                    results = p.map(initiate_transfer, commands)
+                    print(results)
 
-        break
+            time.sleep(wait_time)
 
-  
+            break
 
 
-    # main()
+# main()
 
 if __name__ == '__main__':
     initiate()
