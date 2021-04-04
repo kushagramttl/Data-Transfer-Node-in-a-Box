cbd39020a706ede1a2d86e088e19fe46fff15583
changsu <suchang177@outlook.com>
2021-04-03 21:29:57 -0400

    refactor

--- Network/Controller/portalClient_transfer.py
+++ Network/Controller/portalClient_transfer.py
@@ -1,33 +1,32 @@
 import requests
 
 class PortalClient_transfer:
+    def get_transfer_list(url) :
+        session = requests.Session()
+        respose = session.get(url)
+        print("Get transfers response : ", respose)
+        if respose != None:
+            transfers = respose.json()
+            print("Get transfers : ", transfers)
+            return transfers
+        return {}
 
-def get_transfer_list(url) :
-    session = requests.Session()
-    respose = session.get(url)
-    print("Get transfers response : ", respose)
-    if respose != None:
-        transfers = respose.json()
-        print("Get transfers : ", transfers)
-        return transfers
-    return {}
+    def post_transfer(url, transferId) :
+        transfer_object = {
+            "status" : "Start",
+        }
+        session = requests.Session()
+        header = {"Content-type": "application/json"}
+        response = session.post(url + "/" + transferId, data = transfer_object, headers=header)
+        print("Transfer Response : ", response)
 
-def post_transfer(url, transferId) :
-    transfer_object = {
-        "status" : "Start",
-    }
-    session = requests.Session()
-    header = {"Content-type": "application/json"}
-    response = session.post(url + "/" + transferId, data = transfer_object, headers=header)
-    print("Transfer Response : ", response)
+    def update_status(url, transferId) :
+        transfer_object = {
+            "status" : "In Progress",
+        }
+        session = requests.Session()
+        header = {"Content-type": "application/json"}
+        response = session.put(url + "/" + transferId, data = transfer_object, headers=header)
 
-def update_status(url, transferId) :
-    transfer_object = {
-        "status" : "In Progress",
-    }
-    session = requests.Session()
-    header = {"Content-type": "application/json"}
-    response = session.put(url + "/" + transferId, data = transfer_object, headers=header)
-
-def __init__(self):
-    print("Client initiated!")
+    def __init__(self):
+        print("Client initiated!")
--- Network/controller/main.py
+++ Network/controller/main.py
@@ -1,17 +1,16 @@
-import time
-from transferClient import get_transfer_list, post_transfer, update_status
+from protalClient_transfer import PortalClient_transfer
 from config import ConfigSingleton
 from ContainerScripts.Controller.portalclient import login
 def initiate():
 	ConfigSingleton.getInstance().set_config_values()
     # to start a new transfer
     start_transfer = ConfigSingleton.getInstance().config_dict["INIT_TRANSFER_URL"]
-    
-    post_transfer( start_transfer, transferId)
 
-    transfers = get_transfer_list(ConfigSingleton.getInstance().config_dict["GET_TRANSFER_LIST_URL"])
+    PortalClient_transfer.post_transfer( start_transfer, transferId)
 
-    update_transfer = update_status( ConfigSingleton.getInstance().config_dict["GET_TRANSFER_STATUS_URL"], transferId )
+    transfers = PortalClient_transfer.get_transfer_list(ConfigSingleton.getInstance().config_dict["GET_TRANSFER_LIST_URL"])
+
+    update_transfer = PortalClient_transfer.update_status( ConfigSingleton.getInstance().config_dict["GET_TRANSFER_STATUS_URL"], transferId )
 
 if __name__ == '__main__':
     initiate()
