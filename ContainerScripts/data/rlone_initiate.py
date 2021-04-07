0aa8d85e72956a340d277003be54876760739b6a
changsu <suchang177@outlook.com>
2021-04-07 12:54:56 -0400

    Update rlone_initiate.py

--- ContainerScripts/data/rlone_initiate.py
+++ ContainerScripts/data/rlone_initiate.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def make_alias(endpoint, port, alias_name, access_key, secret_key):
-  endp = "http://" + endpoint + " :" + port
+  endp = "http://" + endpoint + ":" + port
   make_alias_cmd = "rclone config create " + alias_name
   + " s3 env_auth true access_key_id "
   + access_key + " secret_access_key " + secret_key
@@ -9,5 +9,5 @@ def make_alias(endpoint, port, alias_name, access_key, secret_key):
   subprocess.run( make_alias_cmd , shell=True)
 
 def init_transfer( local_dir,  alias_name, bucket_name ):
-  start_transfer_cmd = "rclone copy " + local_dir + " " + alias_name + "/" + "bucket_name"
+  start_transfer_cmd = "rclone copy " + local_dir + " " + alias_name + ":" + "bucket_name"
   subprocess.run( start_transfer_cmd , shell=True)
