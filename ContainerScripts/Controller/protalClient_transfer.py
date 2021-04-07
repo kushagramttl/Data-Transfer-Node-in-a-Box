b68b0c34779f546298916794ace03b6a7ccc1992
kushagramttl <mittal.ku@northeastern.edu>
2021-04-06 22:19:47 -0400

    Transfer class update

--- ContainerScripts/Controller/protalClient_transfer.py
+++ ContainerScripts/Controller/protalClient_transfer.py
@@ -1,7 +1,9 @@
-import requests json
+import requests
+import json
+
 
 class PortalClient_transfer:
-    def get_transfer_list(url, session) :
+    def get_transfer_list(self, url, session):
         respose = session.get(url)
         print("Get transfers response : ", respose)
         if respose != None:
@@ -10,22 +12,22 @@ class PortalClient_transfer:
             return transfers
         return {}
 
-    def post_transfer(url, transferId, session) :
+    def post_transfer(self, url, transferId, session):
         transfer_object = {
-            "status" : "Start",
+            "status": "Start",
         }
         json_object = json.dumps(transfer_object)
         header = {"Content-type": "application/json"}
-        response = session.post(url + "/" + transferId, data = json_object, headers=header)
+        response = session.post(url + "/" + transferId, data=json_object, headers=header)
         print("Transfer Response : ", response)
 
-    def update_status(url, transferId, session) :
+    def update_status(self, url, transferId, session):
         transfer_object = {
-            "status" : "In Progress",
+            "status": "In Progress",
         }
         json_object = json.dumps(transfer_object)
         header = {"Content-type": "application/json"}
-        response = session.put(url + "/" + transferId, data = json_object, headers=header)
+        response = session.put(url + "/" + transferId, data=json_object, headers=header)
 
     def __init__(self):
         print("Client initiated!")
