31f3c0aaff8aaeffa353c89bf0f625016a56acc5
changsu <suchang177@outlook.com>
2021-04-14 20:06:21 -0400

    delete file and fix tiny typo

--- ContainerScripts/data/transfer_with_status_back.py
+++ ContainerScripts/data/transfer_with_status_back.py
@@ -2,7 +2,7 @@ import requests
 import json
 # remote_dir here should contain alias to that endpoint and the bucket to put this file to.
 # ie, alias:bucket
-def init_transfer(local_dir, file_to_send, remote_dir,  local_dir, transferId) :
+def init_transfer(local_dir, file_to_send, remote_dir, transferId) :
     # hard_code params
     '''
     dummy_params_fortest = {
@@ -26,7 +26,7 @@ def init_transfer(local_dir, file_to_send, remote_dir,  local_dir, transferId) :
     header = {
         "Content-type": "application/json"
     }
-    json_object = json.dumps(dummy_params_fortest)
+    json_object = json.dumps(params)
 
     url = "http://localhost:5572/operations/copyfile"
     response=session.post( url, data=json_object, headers=header)
@@ -39,7 +39,7 @@ def init_transfer(local_dir, file_to_send, remote_dir,  local_dir, transferId) :
         "speedAvg":"0",
         "size":"0"
     }
-    return_json=json.dumps(retuen_obj)
+    return_json=json.dumps(return_obj)
     print(response.json())
     print(return_json)
     return return_json
@@ -62,7 +62,7 @@ def get_status( transferId ) :
     param = {
         "group": transferId,
     }
-    json_object = json.dumps(dummy_params_fortest)
+    json_object = json.dumps(param)
     session = requests.Session()
     header = {"Content-type": "application/json"}
     url = "http://localhost:5572/core/stats"
@@ -73,7 +73,11 @@ def get_status( transferId ) :
         transferring = {
             "eta":"0",
             "percentage":"100",
-            "speed": "0"
+            "speed": "0",
+            "name" : "",
+            "bytes" : "0",
+            "size" : "0",
+            "speedAvg" : "0"
         }
         transferring_object = json.dumps(transferring)
         print( "transferring" ,  transferring_object)
